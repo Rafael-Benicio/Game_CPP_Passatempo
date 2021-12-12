10bd77ff9092c1a1f668ec29927123b5a05f3577
Rafael-Benicio <jerobenicio@gmail.com>
2021-12-12 02:32:13 -0300

    remoção das responsabilidades da Classe Node2D

--- class/Node2D.hpp
+++ class/Node2D.hpp
@@ -5,34 +5,15 @@
 class Node2D{
 	public:
 		// Variaveis----------------
-		// Instancia a Textura
-		sf::Texture texture;
-		// Instancia Sprite
-		sf::Sprite sprite;
 		// Corresponde a posição do objeto
 		Position position;
-		// vetor de direção de aceleração
-		Position vector_move;
-
-		Proportion proportion;
 		// Funções------------------
-		// Retorna sprites
-		sf::Sprite drawSP();
 		// Retorna a posição do player
 		sf::Vector2f getPositionCenter();
 		// Configura posição
 		void setPosition(int x, int y);
-		// Modifiica a area visualizavel da imagem
-		void setShowArea(int x,int y,int w,int h);
-		// Configura o sprite e a textura
-		void setSpriteTexture(std::string texture_path);
 
 };
-
-// Chama pra desenhar
-inline sf::Sprite Node2D::drawSP(){ 
-	return sprite; 
-}
 
 // Posição do player
 inline sf::Vector2f Node2D::getPositionCenter(){ 
@@ -43,26 +24,7 @@ inline sf::Vector2f Node2D::getPositionCenter(){
 void Node2D::setPosition(int x,int y){
 	position.x=x;
 	position.y=y;
-	sprite.setPosition(position.x,position.y); 
 }
 
-// Configura area que ira ser mostrada
-inline void Node2D::setShowArea(int x,int y,int w,int h){ 
-	sprite.setTextureRect(sf::IntRect(x,y,w,h)); 
-}
-
-// Configura o sprite e a textura
-void Node2D::setSpriteTexture(std::string texture_path){
-	// Carrega a textura
-	texture.loadFromFile(texture_path);
-	// Coloca a testura no sprite
-	sprite.setTexture(texture);
-	// Configura proportion
-	proportion.width=texture.getSize().x;
-	proportion.height=texture.getSize().y;
-	// configura o sprite no rect
-	sprite.setTextureRect(sf::IntRect(0,0,proportion.width,proportion.height));
-
-}
 
 #endif // CHAR_HPP_INCLUDED
